44a7d01e988f16ea6c47a49e7f71b45c692b47b2
xieyajie <xyjxyf@163.com>
2013-08-31 20:39:39 +0800

    new create project

---
+++ XDPlans/XDPlans/XDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  XDAppDelegate.h
+//  XDPlans
+//
+//  Created by xie yajie on 13-8-31.
+//  Copyright (c) 2013年 XD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class XDViewController;
+
+@interface XDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) XDViewController *viewController;
+
+@end
---
+++ XDPlans/XDPlans/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XDPlans
+//
+//  Created by xie yajie on 13-8-31.
+//  Copyright (c) 2013年 XD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XDAppDelegate class]));
+    }
+}
